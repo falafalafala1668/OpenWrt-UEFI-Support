82b4e8c3493b316607494991895bf9d6631ea948
falafalafala1668 <mianmian1668@gmail.com>
2020-02-01 19:33:53 +0800

    Fix issues

--- RunMe.sh
+++ RunMe.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
-if [ "$1" = apply ]; then
-  patch -p0 < openwrt-uefi-support/Config-images.patch
-  patch -p0 < openwrt-uefi-support/common.patch
-  patch -p0 < openwrt-uefi-support/Image.patch
-  patch -p0 < openwrt-uefi-support/tools.patch
+restore_patch() {
+  patch -p0 -R < OpenWrt-UEFI-Support/Config-images.patch
+  patch -p0 -R < OpenWrt-UEFI-Support/common.patch
+  patch -p0 -R < OpenWrt-UEFI-Support/Image.patch
+  patch -p0 -R < OpenWrt-UEFI-Support/tools.patch
+}
+if [ "$1" = "apply" ]; then
+  restore_patch
+  git pull OpenWrt-UEFI-Support
+  patch -p0 < OpenWrt-UEFI-Support/Config-images.patch
+  patch -p0 < OpenWrt-UEFI-Support/common.patch
+  patch -p0 < OpenWrt-UEFI-Support/Image.patch
+  patch -p0 < OpenWrt-UEFI-Support/tools.patch
   mv package/boot/grub2/Makefile package/boot/grub2/Makefile.bak
-  cp -a openwrt-uefi-support/src/package/boot/grub2/* package/boot/grub2/
-  cp -r openwrt-uefi-support/src/target/linux/x86/image/gen_image_efi.sh target/linux/x86/image/
-  cp -r openwrt-uefi-support/src/tools/gptfdisk tools/
-  cp -r openwrt-uefi-support/src/tools/popt tools/
+  cp -a OpenWrt-UEFI-Support/src/package/boot/grub2/* package/boot/grub2/
+  cp -r OpenWrt-UEFI-Support/src/target/linux/x86/image/gen_image_efi.sh target/linux/x86/image/
+  cp -r OpenWrt-UEFI-Support/src/tools/gptfdisk tools/
+  cp -r OpenWrt-UEFI-Support/src/tools/popt tools/
 elif [ "$1" = "restore" ]; then
-  patch -p0 -R < openwrt-uefi-support/Config-images.patch
-  patch -p0 -R < openwrt-uefi-support/common.patch
-  patch -p0 -R < openwrt-uefi-support/Image.patch
-  patch -p0 -R < openwrt-uefi-support/tools.patch
+  restore_patch
   rm -rf package/boot/grub2/grub2-efi package/boot/grub2/common.mk
   rm -rf tools/gptfdisk tools/popt
   rm -rf target/linux/x86/image/gen_image_efi.sh
   mv package/boot/grub2/Makefile.bak package/boot/grub2/Makefile
 else
-  echo "Please add parameter. Apply or Restore\n e.g: ./openwrt-uefi-support/RunMe.sh apply"
+  echo "Please add parameter. Apply or Restore\n e.g: ./OpenWrt-UEFI-Support/RunMe.sh apply"
 fi
