c91d72dd97fbc5f3cd10a3f98bfd753bf256d084
falafalafala1668 <57652508+falafalafala1668@users.noreply.github.com>
2020-02-12 18:12:02 +0800

    Update RunMe.sh

--- RunMe.sh
+++ RunMe.sh
@@ -1,34 +1,52 @@
 #!/bin/sh
 restore_patch() {
-  patch -p0 -R < OpenWrt-UEFI-Support/Config-images.patch
-  patch -p0 -R < OpenWrt-UEFI-Support/common.patch
-  patch -p0 -R < OpenWrt-UEFI-Support/Image.patch
-  patch -p0 -R < OpenWrt-UEFI-Support/tools.patch
+  patch -REp0 < OpenWrt-UEFI-Support/Config-images.patch
+#  patch -p0 < OpenWrt-UEFI-Support/common.patch
+  patch -REp0 < OpenWrt-UEFI-Support/Image.patch
+  patch -REp0 < OpenWrt-UEFI-Support/tools.patch
+  patch -REp0 < OpenWrt-UEFI-Support/Grub-Makefile.patch
 }
 
 update() {
-  echo "Updating patches"
-  pushd OpenWrt-UEFI-Support
+  echo "###### Updating patches ######"
+  cd OpenWrt-UEFI-Support
   git pull
-  popd
+  cd ..
 }
-if [ "$1" = "apply" ]; then
-  update
+
+generate_patch() {
+  diff -Naur package/boot/grub2/Makefile OpenWrt-UEFI-Support/src/package/boot/grub2/Makefile > OpenWrt-UEFI-Support/Grub-Makefile.patch
+  diff -Naur config/Config-images.in OpenWrt-UEFI-Support/src/config/Config-images.in > OpenWrt-UEFI-Support/Config-images.patch
+  diff -Naur tools/Makefile OpenWrt-UEFI-Support/src/tools/Makefile > OpenWrt-UEFI-Support/tools.patch
+  diff -Naur target/linux/x86/image/Makefile OpenWrt-UEFI-Support/src/target/linux/x86/image/Makefile > OpenWrt-UEFI-Support/Image.patch
+}
+
+case "$1" in
+  "apply")
+  echo "###### Applying patches ######"
   patch -p0 < OpenWrt-UEFI-Support/Config-images.patch
-  patch -p0 < OpenWrt-UEFI-Support/common.patch
+#  patch -p0 < OpenWrt-UEFI-Support/common.patch
   patch -p0 < OpenWrt-UEFI-Support/Image.patch
   patch -p0 < OpenWrt-UEFI-Support/tools.patch
-  mv package/boot/grub2/Makefile package/boot/grub2/Makefile.bak
-  cp -a OpenWrt-UEFI-Support/src/package/boot/grub2/* package/boot/grub2/
+  patch -p0 < OpenWrt-UEFI-Support/Grub-Makefile.patch
+  cp -a OpenWrt-UEFI-Support/src/package/boot/grub2/grub2-efi package/boot/grub2/
   cp -r OpenWrt-UEFI-Support/src/target/linux/x86/image/gen_image_efi.sh target/linux/x86/image/
   cp -r OpenWrt-UEFI-Support/src/tools/gptfdisk tools/
   cp -r OpenWrt-UEFI-Support/src/tools/popt tools/
-elif [ "$1" = "restore" ]; then
+  ;;
+  "restore")
   restore_patch
   rm -rf package/boot/grub2/grub2-efi package/boot/grub2/common.mk
   rm -rf tools/gptfdisk tools/popt
   rm -rf target/linux/x86/image/gen_image_efi.sh
-  mv package/boot/grub2/Makefile.bak package/boot/grub2/Makefile
-else
+  ;;
+  "generate")
+  generate_patch
+  ;;
+  "update")
+  update
+  ;;
+  *)
   echo "Please add parameter. Apply or Restore\n e.g: ./OpenWrt-UEFI-Support/RunMe.sh apply"
-fi
+  ;;
+esac
